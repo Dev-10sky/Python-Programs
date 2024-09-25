bf906ed76616b71a3b18f7a1b64c4c4567236dc3
Dev-10sky <69281898+Dev-10sky@users.noreply.github.com>
2024-09-25 10:02:23 -0400

    Fixed command line display issues

--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -2,17 +2,22 @@ import math
 
 def printDisplay(a):
 	for n in a:
-		print(n)
+		temp = ''.join(n)
+		print(temp)
+	print("<----------------------------------------------->")
 
 def makeDisplay(maxval,arrlen):
-	return [[0]*(arrlen)]*(maxval)
+	temp = [["  " for n in range(arrlen)] for x in range(maxval)]
+	return temp
 
 def updateDisplay(display,arr):
 	for val in range(len(arr)):
 		for row in range(len(display)):
 			res = (len(display) - (row)) - arr[val]
 			if res <= 0:
-				display[row][val] = 1
+				display[row][val] = "[]"
+			else:
+				display[row][val] = "  "
 				
 def bubble(lst):
 	for m in range(len(lst)-1):
@@ -21,15 +26,21 @@ def bubble(lst):
 				lst[n], lst[n + 1]  = lst[n + 1], lst[n]
 	return lst
 
-
-
+def bubbleDisplay(arr):
+	currMax = max(arr)
+	size = len(arr)
+	window = makeDisplay(currMax,size)
+	print(arr)
+	updateDisplay(window,arr)
+	printDisplay(window)
+	for m in range(len(arr)-1):
+		for n in range(len(arr)-1):
+			if arr[n] > arr[n + 1]:
+				arr[n], arr[n + 1]  = arr[n + 1], arr[n]
+				updateDisplay(window,arr)
+				printDisplay(window)
+	return arr
 
 if __name__ == '__main__':
 	l = [7, 4, 8, 6, 2 ,1 ,9, 10, 5]
-	print(l)
-	# print(bubble(l))
-	newDisplay = makeDisplay(max(l), len(l))
-	printDisplay(newDisplay)
-	updateDisplay(newDisplay,l)
-	print("<----------------------------------------------->")
-	printDisplay(newDisplay)
+	print(bubbleDisplay(l))
