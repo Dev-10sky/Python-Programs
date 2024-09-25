cd390e98514640676835e76149517edd52a6f305
Dev-10sky <69281898+Dev-10sky@users.noreply.github.com>
2024-09-25 10:38:48 -0400

    Added __init__ for packages and created Display Class for sorts

--- box/box.py
+++ box/box.py
@@ -16,7 +16,34 @@ class Box:
         sides = side * self.height
         res = top + sides + top
         return res
+
+class Display:
+
+    def __init__(self, maxVal, arrLen) -> None:
+        self.max = maxVal
+        self.length = arrLen
+        self.window = None
     
+    def makeDisplay(self):
+        self.window = [["  " for n in range(self.length)] for x in range(self.max)]
+
+    def printDisplay(self):
+        line = len(self.window[0])
+        for n in self.window:
+            temp = ''.join(n)
+            print(temp)
+        print("--"*line)
+    
+    def updateDisplay(self,arr):
+        for val in range(len(arr)):
+            for row in range(len(self.window)):
+                res = (len(self.window) - (row)) - arr[val]
+                if res <= 0:
+                    self.window[row][val] = "[]"
+                else:
+                    self.window[row][val] = "  "
+
+
 if __name__ == '__main__':
     print("Hello World!")
     boxOne = Box(3,6,1)
--- sorts/bubble_sort.py
+++ sorts/bubble_sort.py
@@ -1,25 +1,6 @@
 import math
+from box.box import Display
 
-def printDisplay(a):
-	line = len(a[0])
-	for n in a:
-		temp = ''.join(n)
-		print(temp)
-	print("--"*line)
-
-def makeDisplay(maxval,arrlen):
-	temp = [["  " for n in range(arrlen)] for x in range(maxval)]
-	return temp
-
-def updateDisplay(display,arr):
-	for val in range(len(arr)):
-		for row in range(len(display)):
-			res = (len(display) - (row)) - arr[val]
-			if res <= 0:
-				display[row][val] = "[]"
-			else:
-				display[row][val] = "  "
-				
 def bubble(lst):
 	for m in range(len(lst)-1):
 		for n in range(len(lst)-1):
@@ -27,21 +8,19 @@ def bubble(lst):
 				lst[n], lst[n + 1]  = lst[n + 1], lst[n]
 	return lst
 
-def bubbleDisplay(arr):
-	currMax = max(arr)
-	size = len(arr)
-	window = makeDisplay(currMax,size)
-	print(arr)
-	updateDisplay(window,arr)
-	printDisplay(window)
+def bubbleDisplay(arr,display:Display):
+	display.makeDisplay()
+	display.updateDisplay(arr)
+	display.printDisplay()
 	for m in range(len(arr)-1):
 		for n in range(len(arr)-1):
 			if arr[n] > arr[n + 1]:
 				arr[n], arr[n + 1]  = arr[n + 1], arr[n]
-				updateDisplay(window,arr)
-				printDisplay(window)
+				display.updateDisplay(arr)
+				display.printDisplay()
 	return arr
 
 if __name__ == '__main__':
 	l = [7, 4, 8, 6, 2 ,1 ,9, 10, 5]
-	print(bubbleDisplay(l))
+	dispOne = Display(max(l),len(l))
+	print(bubbleDisplay(l,dispOne))
